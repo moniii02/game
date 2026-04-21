4356cd57af332be1bfb55e7323ba00d398e18f77
Monika Khan <monika.khan@itiszuccante.edu.it>
2026-04-21 16:10:10 +0200

    background, enemies spawing

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ screen_title = "GAME"
 
 gravity = 1 
 jump_speed = 18
-player_speed = 6
+player_speed = 6.5
 
 class Player(SpriteAnimato):
     def __init__(self):
@@ -45,7 +45,7 @@ class Player(SpriteAnimato):
         
 class Enemy(SpriteAnimato):
     def __init__(self):
-        super().__init__(scala = 1.5)
+        super().__init__(scala = 1.05)
         file_animazione = {
             "destra": "./assets/run_slime.png",
             "sinistra": "./assets/run_slime.png"
@@ -82,8 +82,6 @@ class Enemy(SpriteAnimato):
 class GAME (arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-
-        arcade.set_background_color(arcade.color.SKY_BLUE)
         
         #player
         self.player = None
@@ -121,6 +119,9 @@ class GAME (arcade.Window):
         
 
     def setup(self):
+
+        self.background = arcade.load_texture("./assets/background.png")
+
         #player
         self.player = Player()
         self.player.center_x = 75
@@ -131,14 +132,14 @@ class GAME (arcade.Window):
         ground = arcade.SpriteSolidColor(60000, 40)
         ground.center_x = 2500
         ground.center_y = 20
-        ground.color = arcade.color.DARK_GREEN
+        ground.color = arcade.color.OLD_HELIOTROPE
         self.wall_list.append(ground)
 
         #first platfrom manually
-        platform = arcade.SpriteSolidColor(135, 20, arcade.color.BROWN)
+        platform = arcade.SpriteSolidColor(160, 20)
         platform.center_x = 115
         platform.center_y = 155
-        platform.color = arcade.color.BROWN
+        platform.color = arcade.color.JAPANESE_VIOLET
         self.wall_list.append(platform)
 
         #initial platforms
@@ -152,29 +153,36 @@ class GAME (arcade.Window):
         width = random.randint(120, 220)
         x = self.last_platform_x + random.randint(180, 300)
         y = random.randint(120, 300)
+        y = min(max(y, 100), 320)
 
-        platform = arcade.SpriteSolidColor(width, 20, arcade.color.BROWN)
+        platform = arcade.SpriteSolidColor(width, 20)
+        platform.occupied = False
         platform.center_x = x
         platform.center_y = y
         self.last_platform_x = x
-        platform.color = arcade.color.BROWN
+        platform.color = arcade.color.JAPANESE_VIOLET
         self.wall_list.append(platform)
 
         #coins
         if random.random() < 0.5:
-            coin = arcade.Sprite("./assets/coin.png", scale= 0.15)
+            coin = arcade.Sprite("./assets/coin.png", scale= 0.1)
             coin.center_x = x + random.randint(-60, 60)
             coin.bottom = y + random.randint(40, 120)
             self.coin_list.append(coin)
 
     def setup_enemy(self):
         enemy = Enemy()
-        platform = random.choice(self.wall_list)
+        #enemy spawns only on platforms
+        valid_platform = [p for p in self.wall_list if p.center_x > self.player.center_x + 300 and p.center_y > 80 and not p.occupied]
+        if not valid_platform:
+            return
+        platform = random.choice(valid_platform)
+        platform.occupied = True
         enemy.center_x = platform.center_x 
         enemy.bottom = platform.top
         enemy.boundary_left = platform.left
         enemy.boundary_right = platform.right
-        enemy.change_x = -2
+        enemy.change_x = -1
         self.enemy_list.append(enemy)
 
     def center_camera_to_player(self):
@@ -183,6 +191,8 @@ class GAME (arcade.Window):
     def on_draw(self):
         self.clear()
 
+        arcade.draw_texture_rect(self.background, arcade.LBWH(0, 0, screen_width, screen_height))
+
         #World
         with self.camera.activate():
             self.wall_list.draw()
@@ -190,23 +200,22 @@ class GAME (arcade.Window):
             self.enemy_list.draw()
             self.coin_list.draw()
         
-        arcade.draw_text(f"score: {int(self.score)}", 10, screen_height - 30, arcade.color.WHITE, 20)
+        arcade.draw_text(f"score: {int(self.score)}", 10, screen_height - 30, arcade.color.RICH_LILAC, 20)
 
         if self.game_over:
-            arcade.draw_text("GAME OVER", screen_width // 2 - 135, screen_height // 2, arcade.color.DARK_RED, 40)
-            
+            arcade.draw_text("GAME OVER", screen_width // 2 - 135, screen_height // 2, arcade.color.DARK_RED, 40)       
 
     def on_update(self, delta_time):
         if self.game_over:
             return
-        
-        self.enemy_list.update_animation()
-        self.player_list.update_animation()
+        self.enemy_list.update()
+        self.enemy_list.update_animation(delta_time)
+        self.player_list.update_animation(delta_time)
         
         #score increases over time 
         self.score = self.player.center_x / 30 + self.coin_score
 
-     #player movements
+        #player movements
         if self.move_left:
             self.player.change_x = -player_speed
         elif self.move_right:
@@ -227,7 +236,7 @@ class GAME (arcade.Window):
 
         #spawn enemies 
         self.spawn_timer += delta_time
-        if self.spawn_timer > 3 and  len(self.enemy_list) < 3:
+        if self.spawn_timer > 3 and  len(self.enemy_list) < 5:
             self.setup_enemy()
             self.spawn_timer = 0
         
@@ -241,7 +250,7 @@ class GAME (arcade.Window):
                 enemy.change_x *= -1
 
             #remove off-screen enemies
-            if enemy.right < 0:
+            if enemy.right < self.player.center_x - screen_width:
                 enemy.remove_from_sprite_lists()
 
             #collision
